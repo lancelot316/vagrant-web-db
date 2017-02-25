1d0cdf949d1aa30e43ce148665ded741e9dc2d02
Lance Newman <lancelot316@msn.com>
2017-02-25 14:20:54 -0500

    updated the box to ubuntu/xenial64 and mysql to 5.7

--- db_provision.sh
+++ db_provision.sh
@@ -1,6 +1,6 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y mysql-server
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/my.cnf
-sudo service mysql restart
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo systemctl restart mysql
 sudo mysql <<< "GRANT ALL ON *.* TO 'root'@'192.168.33.10' identified by 'root'; FLUSH PRIVILEGES;"
